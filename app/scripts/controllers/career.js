bbb26724697d7e9c7bcaab36b74ad95f9d1bc589
Jonnie Spratley <jonniespratley@gmail.com>
2013-07-15 15:42:12 -0700

    updated some logic on health

--- app/scripts/controllers/career.js
+++ app/scripts/controllers/career.js
@@ -1,189 +1,231 @@
 'use strict';
-angular.module('myBoardApp').controller('CareerCtrl', function ($scope, $rootScope, ParseService, App) {
-	
+angular.module('myBoardApp').controller('CareerCtrl', function($scope, $rootScope, ParseService, App) {
+
 	//Income object
 	$scope.income = {
-		title: null,
-		amount: null,
-		date: null,
-		category: null,
-		color: null
+		id : null,
+		objectId : null,
+		ACL : null,
+		title : null,
+		amount : null,
+		date : null,
+		category : null,
+		color : null
 	};
-	
+
 	//Asset object
 	$scope.asset = {
-		title: null,
-		amount: null,
-		date: null,
-		category: null,
-		color: null
+		title : null,
+		amount : null,
+		date : null,
+		category : null,
+		color : null
 	};
-	
+
 	//Expense object
 	$scope.expense = {
-		title: '',
-		amount: '',
-		date: null,
-		category: '',
-		color: null
+		title : '',
+		amount : '',
+		date : null,
+		category : '',
+		color : null
 	};
-	
+
 	//Net Worth object
 	$scope.totals = {
-		income: 0,
-		assets: 0,
-		expenses: 0
+		income : 0,
+		assets : 0,
+		expenses : 0
 	};
-	
+
 	//View Controller
 	$scope.Career = {
-		data: {
-			expenses: null,
-			income: null,
-			assets: null,
-			assetTotal: 0,
-			expenseTotal: 0,
-			incomeTotal: 0
+		data : {
+			expenses : null,
+			income : null,
+			assets : null,
+			assetTotal : 0,
+			expenseTotal : 0,
+			incomeTotal : 0
 		},
-		chartdata:{
-			expenses: [
-				{ color: '#3A8A56', value: 55 }
-			],
-			income: [],
-			assets: [
-				{ color: '#3A8A56', value: 25 }
-			],
-			networth: {
+		chartdata : {
+			expenses : [{
+				color : '#3A8A56',
+				value : 55
+			}],
+			income : [],
+			assets : [{
+				color : '#3A8A56',
+				value : 25
+			}],
+			networth : {
 				labels : ['Income', 'Expenses'],
-					datasets : [
-						{
-							fillColor : "rgba(11,126,39,0.38)",
-							strokeColor : "rgba(21,133,95,0.6)",
-							pointColor : "rgba(220,220,220,1)",
-							pointStrokeColor : "#fff",
-							data : [65,59]
-						}
-					]
+				datasets : [{
+					fillColor : "rgba(11,126,39,0.38)",
+					strokeColor : "rgba(21,133,95,0.6)",
+					pointColor : "rgba(220,220,220,1)",
+					pointStrokeColor : "#fff",
+					data : [65, 59]
+				}]
 			}
 		},
-		init: function(){
+		init : function() {
 			this.getIncome();
 			this.getAssets();
 			this.getExpenses();
 		},
-		
-		addItem: function(obj){
-			$scope.$apply(function(){
+		addItem : function(obj) {
+			$scope.$apply(function() {
 				$scope.Career.chartdata.assets.push(obj);
 				$scope.Career.chartdata.expenses.push(obj);
 			});
 		},
-		
-		getExpenses: function(){
+		getExpenses : function() {
 			var self = this;
-			ParseService.get('Expense', null, function(data){
-					$scope.Career.data.expenses = data;
-					self.buildExpenseChart();
-					self.calcTotals();
+			ParseService.get('Expense', null, function(data) {
+				$scope.Career.data.expenses = data;
+				self.buildExpenseChart();
+				self.calcTotals();
 			});
 		},
-		getAssets: function(){
+		getAssets : function() {
 			var self = this;
-			ParseService.get('Asset', null, function(data){
-					$scope.Career.data.assets = data;
-					self.buildAssetChart();
+			ParseService.get('Asset', null, function(data) {
+				$scope.Career.data.assets = data;
+				self.buildAssetChart();
 			});
 		},
-		getIncome: function(){
-			ParseService.get('Income', null, function(data){
-				$scope.$apply(function(){
+		getIncome : function() {
+			ParseService.get('Income', null, function(data) {
+				$scope.$apply(function() {
 					$scope.Career.data.income = data;
 				});
 			});
 		},
-		addIncome: function(o){
-			ParseService.add('Income', o, function(data){
-				$scope.$apply(function(){
+		addIncome : function(o) {
+			console.log(o);
+			ParseService.save('Income', o, function(data) {
+				$scope.$apply(function() {
+					$scope.income = {};
 					$scope.Career.getIncome();
 					angular.element('#addIncomeModal').modal('toggle');
 				});
+			}, function(e) {
+				$scope.income = {};
+				alert(e.error);
 			});
 		},
-		addExpense: function(o){
-			ParseService.add('Expense', o, function(data){
-				$scope.$apply(function(){
+		addExpense : function(o) {
+			ParseService.add('Expense', o, function(data) {
+				$scope.$apply(function() {
 					$scope.Career.getExpenses();
 					angular.element('#addExpenseModal').modal('toggle');
 				});
+			}, function(e) {
+				$scope.expense = {};
+				alert(e.error);
 			});
 		},
-		addAsset: function(o){
-			ParseService.add('Asset', o, function(data){
-				$scope.$apply(function(){
+		addAsset : function(o) {
+			ParseService.add('Asset', o, function(data) {
+				$scope.$apply(function() {
 					$scope.Career.chartdata.assets.push(o);
 					$scope.Career.getAssets();
 					angular.element('#addAssetModal').modal('toggle');
 				});
+			}, function(e) {
+				$scope.asset = {};
+				alert(e.error);
 			});
 		},
-		
-		//Calculate totals for net worth chart - 
-		calcTotals: function(){
+		selectAsset : function(o) {
+			$scope.asset = o;
+		},
+		selectExpense : function(o) {
+			$scope.expense = o;
+		},
+		selectIncome : function(o) {
+			console.log(o);
+			$scope.income = o;
+		},
+		destroyAsset : function(o) {
+			ParseService.destroy('Asset', o, function(data) {
+				console.log(data);
+			}, function(e) {
+				alert(e.error);
+			});
+		},
+		destroyExpense : function(o) {
+			ParseService.destroy('Expense', o, function(data) {
+				console.log(data);
+			}, function(e) {
+				alert(e.error);
+			});
+		},
+		destroyIncome : function(o) {
+			ParseService.destroy('Income', o, function(data) {
+				console.log(data);
+			}, function(e) {
+				alert(e.error);
+			});
+
+		},
+		//Calculate totals for net worth chart -
+		calcTotals : function() {
 			var self = this;
 			//Loop all expenses, assets, and income, total up the amount field.
 			var expenseTotal = 0;
 			var assetTotal = 0;
 			var incomeTotal = 0;
-			
-			angular.forEach($scope.Career.data.assets, function(obj){
+
+			angular.forEach($scope.Career.data.assets, function(obj) {
 				assetTotal += parseFloat(obj.amount);
 			});
-			angular.forEach($scope.Career.data.expenses, function(obj){
+			angular.forEach($scope.Career.data.expenses, function(obj) {
 				expenseTotal += parseFloat(obj.amount);
 			});
-			angular.forEach($scope.Career.data.income, function(obj){
+			angular.forEach($scope.Career.data.income, function(obj) {
 				incomeTotal += parseFloat(obj.amount);
 			});
-			
+
 			$scope.Career.data.incomeTotal = incomeTotal;
 			$scope.Career.data.expenseTotal = expenseTotal;
 			$scope.Career.data.assetTotal = assetTotal;
-			$scope.Career.chartdata.networth.datasets[0].data = [ incomeTotal, expenseTotal ];
-			
-			
-			
+			$scope.Career.chartdata.networth.datasets[0].data = [incomeTotal, expenseTotal];
+
 		},
-		
 		//Build the chart data for asset chart
-		buildAssetChart: function(){
+		buildAssetChart : function() {
 			var data = $scope.Career.data.assets;
 			$scope.Career.chartdata.assets = [];
-			angular.forEach(data, function(obj){
-				$scope.$apply(function(){
-					$scope.Career.chartdata.assets.push({ color: String(obj.color), value: Number(obj.amount) });
+			angular.forEach(data, function(obj) {
+				$scope.$apply(function() {
+					$scope.Career.chartdata.assets.push({
+						color : String(obj.color),
+						value : Number(obj.amount)
+					});
 				});
 			});
 		},
-		
 		//Build the chart data for the expenses chart
-		buildExpenseChart: function(){
+		buildExpenseChart : function() {
 			var data = $scope.Career.data.expenses;
 			$scope.Career.chartdata.expenses = [];
-			angular.forEach(data, function(obj){
-				$scope.$apply(function(){
-					$scope.Career.chartdata.expenses.push({ color: String(obj.color), value: Number(obj.amount) });
+			angular.forEach(data, function(obj) {
+				$scope.$apply(function() {
+					$scope.Career.chartdata.expenses.push({
+						color : String(obj.color),
+						value : Number(obj.amount)
+					});
 				});
 			});
 		},
-		
-		
-		
 		//Build the charts from the data returned
-		buildChart: function(name, data){
-			
+		buildChart : function(name, data) {
+
 		}
 	};
-	
+
 	window.Career = $scope.Career;
 	console.log($scope);
 });
--- app/scripts/services/ParseService.js
+++ app/scripts/services/ParseService.js
@@ -1,95 +1,110 @@
 'use strict';
 
-angular.module('myBoardApp').factory('ParseService', [ '$q', function ($q) {
-		Parse.initialize("EWJAR5Zkq2VI2XHfPe3vZnc1LYG5eDcbYt0dHwI5", "stymT2WbAK64RXXwhigTHg1HwxuhFAyOWBGXS2LD");
-		var ParseService = function(){
-			return {
-				get: function(model, params, successCb, errorCb){
-					var	query = new Parse.Query(model),
-							delay = $q.defer(),
-							data = null;
-							if(params){
-								//query.greaterThan(params.name, params.value);		
-							}
-						// Retrieve the most recent ones
-						query.descending("createdAt");
-						query.find({
-							success: function(results) {
-								data = results.map(function(obj){
-									return {	
-										id: obj.id,
-										title: obj.get('title'),
-										body: obj.get('body'),
-										duration: obj.get('duration'),
-										day: obj.get('day'),
-										meal: obj.get('meal'),
-										intensity: obj.get('intensity'),
-										user_id: obj.get('user_id'),
-										calories: obj.get('calories'),
-										date: obj.get('date'),
-										amount: obj.get('amount'),
-										color: obj.get('color'),
-										created: obj.createdAt,
-										updated: obj.updatedAt
-									}
-								});
-								if(successCb){
-									successCb(data);
-								} else {
-									delay.resolve(data);
-								}
-							},
-							error: function(error) {
-								if(errorCb){
-									errorCb(error);
-								}
-								delay.reject(error);
+angular.module('myBoardApp').factory('ParseService', ['$q',
+function($q) {
+	Parse.initialize("EWJAR5Zkq2VI2XHfPe3vZnc1LYG5eDcbYt0dHwI5", "stymT2WbAK64RXXwhigTHg1HwxuhFAyOWBGXS2LD");
+	var ParseService = function() {
+		return {
+			get : function(model, params, successCb, errorCb) {
+				var query = new Parse.Query(model), delay = $q.defer(), data = null;
+				if(params) {
+					//query.greaterThan(params.name, params.value);
+				}
+				// Retrieve the most recent ones
+				query.descending("createdAt");
+				query.find({
+					success : function(results) {
+						data = results.map(function(obj) {
+							return {
+								id : obj.id,
+								title : obj.get('title'),
+								body : obj.get('body'),
+								duration : obj.get('duration'),
+								day : obj.get('day'),
+								meal : obj.get('meal'),
+								intensity : obj.get('intensity'),
+								user_id : obj.get('user_id'),
+								calories : obj.get('calories'),
+								date : obj.get('date'),
+								amount : obj.get('amount'),
+								color : obj.get('color'),
+								created : obj.createdAt,
+								updated : obj.updatedAt
 							}
 						});
-					return delay.promise;
-				},
-				destroy: function(model, data, success, error){
-					var ParseObject = Parse.Object.extend(model);
-					var _parseObject = new ParseObject();
-					console.log('destroy', model, data);
-
-					_parseObject.destroy(data, {
-					  success: function(obj) {
-							console.log(obj);
-					    // The object was deleted from the Parse Cloud.
-								if(success){
-									success(obj);
-								}
-					  },
-					  error: function(myObject, err) {
-							alert(err);
-					    // error is a Parse.Error with an error code and description.
-								if(error){
-									error(err);
-								}
-					  }
-					});
-				},
-				add: function(model, data, success, error){
-					//add user to this data object
-					var ParseObject = Parse.Object.extend(model);
-					var _parseObject = new ParseObject();
-							_parseObject.setACL(new Parse.ACL(Parse.User.current()));
-							_parseObject.save(data, {
-								success: function(object) {
-									if(success){
-										success(object);
-									}
-								},
-								error: function(err){
-									if(error){
-										error(err);
-									}
-								}
-							});
+						if(successCb) {
+							successCb(data);
+						} else {
+							delay.resolve(data);
 						}
-				}
+					},
+					error : function(error) {
+						if(errorCb) {
+							errorCb(error);
+						}
+						delay.reject(error);
+					}
+				});
+				return delay.promise;
+			},
+			destroy : function(model, data, success, error) {
+				var ParseObject = Parse.Object.extend(model);
+				var _parseObject = new ParseObject();
+				console.log('destroy', model, data);
+
+				_parseObject.destroy(data, {
+					success : function(obj) {
+						console.log(obj);
+						// The object was deleted from the Parse Cloud.
+						if(success) {
+							success(obj);
+						}
+					},
+					error : function(myObject, err) {
+						alert(err);
+						// error is a Parse.Error with an error code and description.
+						if(error) {
+							error(err);
+						}
+					}
+				});
+			},
+			add : function(model, data, success, error) {
+				//add user to this data object
+				var ParseObject = Parse.Object.extend(model);
+				var _parseObject = new ParseObject();
+				_parseObject.setACL(new Parse.ACL(Parse.User.current()));
+				_parseObject.save(data, {
+					success : function(object) {
+						if(success) {
+							success(object);
+						}
+					},
+					error : function(err) {
+						if(error) {
+							error(err);
+						}
+					}
+				});
+			},
+			save : function(model, data, success, error) {
+				var ParseObject = Parse.Object.extend(model);
+				var _parseObject = new ParseObject();
+				_parseObject.setACL(new Parse.ACL(Parse.User.current()));
+				_parseObject.save(data, {
+					success : function(object) {
+						if(success) {
+							success(object);
+						}
+					},
+					error : function(err) {
+						if(error) {
+							error(err);
+						}
+					}
+				});
 			}
-		
-		return ParseService();
+		}
+	}
+	return ParseService();
 }]);
