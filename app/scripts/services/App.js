5adedd866a0b9d83262c883cb71aa21d5198e38a
Jonnie Spratley <jonniespratley@gmail.com>
2013-07-18 17:49:52 -0700

    updated user email

--- app/scripts/services/App.js
+++ app/scripts/services/App.js
@@ -122,15 +122,15 @@ function($rootScope, $location) {
 
 	App.user = {
 		username : App.currentUser.get('username'),
-		email : App.currentUser.get('email'),
+		email : App.currentUser.get('email') ? App.currentUser.get('email') : '',
 		gender : App.currentUser.get('gender'),
 		height : App.currentUser.get('height'),
 		name : App.currentUser.get('name'),
 		calories : App.currentUser.get('calories'),
 		activity : App.currentUser.get('activity'),
-		weight : App.currentUser.get('weight'),
-		icon : 'http://gravatar.com/avatar/' + calcMD5(App.currentUser.get('email')) + '?s=35.png',
-		avatar : 'http://gravatar.com/avatar/' + calcMD5(App.currentUser.get('email')) + '?s=250.png'
+		weight : App.currentUser.get('weight')
 	};
+	App.user.icon = 'http://gravatar.com/avatar/' + calcMD5(App.user.email) + '?s=35.png';
+	App.user.avatar = 'http://gravatar.com/avatar/' + calcMD5(App.user.email) + '?s=250.png'
 	return App;
 }]);
